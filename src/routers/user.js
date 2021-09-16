27e04d68749899d55292f173610b1e6a75c39133
Colin Francis <francico@oregonstate.edu>
2021-09-16 02:32:18 -0400

    Create user endpoint created

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,7 +9,18 @@ const User = require('../models/user.js');
 const router = new express.Router();
 
 // Endpoint - Create user account:
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
 
+    try {
+        const token = await user.generateAuthToken();
+        res.status(201).send({user, token});
+    } catch(error) {
+        res.status(500).send(error);
+    }
+});
+
+// Endpoint - Login user:
 
 // Exports:
 module.exports = router;
