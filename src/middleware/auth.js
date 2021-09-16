63f597b0b699b8d6d8a359a1f2105d6a95407b29
Colin Francis <francico@oregonstate.edu>
2021-09-16 03:15:57 -0400

    Created auth middleware. Completed get user profile endpoint.

---
+++ src/middleware/auth.js
@@ -0,0 +1,23 @@
+// Author: Colin Francis
+// Description: Authorization middleware
+
+// Imports:
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// Authenticate user:
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'puhvp;ow#@R@;kjnfw');
+        const user = await User.findOne({id: decoded.id, 'tokens.token': token});
+
+        req.user = user;
+        next();
+    } catch(error) {
+        res.send(401).send({error: 'Please authenticate'});
+    }
+}
+
+// Exports:
+module.exports = auth;
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,7 @@
 // Imports:
 const express = require('express');
 const User = require('../models/user.js');
+const auth = require('../middleware/auth.js');
 
 // Initialize new router:
 const router = new express.Router();
@@ -24,10 +25,16 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.verifyUser(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({user, token});
     } catch(error) {
         res.status(400).send();
     }
+});
+
+// Endpoint - Get user profile:
+router.get('/users/profile', auth, async (req, res) => {
+    res.send(req.user);
 })
 
 // Exports:
